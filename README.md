87c3b80976cd7cd48ac9998effb5efaa1afa1673
Mohamed Radhi Guennichi <rguennichi@echo.tn>
2019-01-21 15:17:34 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Go MYSQL Dump
+Create MYSQL dumps on Symfony projects in Golang, by parsing parameters.yml file to get database connection parameters.
+
+### Usage Example
+```console
+$ go build export.go
+$ ./export /path/to/parameters.yml /path/to/dumps
+```
---
+++ export.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/JamesStewy/go-mysqldump"
+	_ "github.com/go-sql-driver/mysql"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"os"
+)
+
+type ConfigParamsYAML struct {
+	Parameters struct {
+		DatabaseHost     string `yaml:"database_host"`
+		DatabasePort     string `yaml:"database_port"`
+		DatabaseName     string `yaml:"database_name"`
+		DatabaseUser     string `yaml:"database_user"`
+		DatabasePassword string `yaml:"database_password"`
+	}
+}
+
+func main() {
+	// Fetch args
+	// parameters.yml file from symfony project
+	yamlFile := os.Args[1]
+	// Directory contains the backups
+	dumpDir := os.Args[2]
+
+	yamlParams := ConfigParamsYAML{}
+	// Read YAML configuration file to get db connection.
+	b, err := ioutil.ReadFile(yamlFile)
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	// Parse the YAML file into the ConfigParamsYAML object.
+	err = yaml.Unmarshal(b, &yamlParams)
+
+	if err != nil {
+		panic(err)
+	}
+
+	parameters := yamlParams.Parameters
+
+	// Check if the port is empty
+	if len(parameters.DatabasePort) == 0 {
+		parameters.DatabasePort = "3306"
+	}
+
+	// Accepts time layout string and add .sql at the end of file
+	dumpFilenameFormat := fmt.Sprintf("%s-20060102T150405", parameters.DatabaseName)
+
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
+		parameters.DatabaseUser,
+		parameters.DatabasePassword,
+		parameters.DatabaseHost,
+		parameters.DatabasePort,
+		parameters.DatabaseName))
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Create the directory
+	if _, err := os.Stat(dumpDir); os.IsNotExist(err) {
+		_ = os.Mkdir(dumpDir, os.ModePerm)
+	}
+
+	// Register database with mysqldump command
+	dumper, err := mysqldump.Register(db, dumpDir, dumpFilenameFormat)
+	if err != nil {
+		panic(err)
+	}
+
+	// Dump database to file
+	resultFilename, err := dumper.Dump()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("File is saved to '%s'.\n", resultFilename)
+
+	// Close dumper and connected database
+	_ = dumper.Close()
+}
